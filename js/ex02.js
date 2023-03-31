325bd48772aec0183c49a8f4cf387f5f22c31077
J. Rodriguez <>
2023-03-31 03:49:45 -0400

    Finish ex02

---
+++ js/ex02.js
@@ -0,0 +1,17 @@
+// This is a comment, everything after is ignored.
+
+console.log("I am Code");
+
+/* This is also a comment,
+   you use this for longer things
+   you want to say.
+*/
+
+console.log("So am I");
+
+// The code inside the next comment will not run.
+
+/* console.log("I will not run") */
+console.log("but I will");
+
+
