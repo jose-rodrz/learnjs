345e3b0f46b1524475800e58494254ab7742240b
J. Rodriguez <>
2023-04-04 03:17:25 -0400

    Add solution to exercise 6 (ex06.js)

---
+++ js/ex06.js
@@ -0,0 +1,6 @@
+let talking = "I'm not sure if she said, \"Hello\" or not.";
+let walking = 'I\'ll need to go \'cause you don\'t know.';
+
+// these strings are broken. Use escapes to fix them.
+let height = "6'2\"";
+console.log('I\'m not g\'na see y\'ll for a while.');
