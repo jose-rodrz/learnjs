0a4fb2546aef99e1545f013916aab3497d32365f
J. Rodriguez <>
2023-05-04 05:47:26 -0400

    Simplify the constructor function and delete example code

--- js/ex21.js
+++ js/ex21.js
@@ -1,14 +1,11 @@
-function createPerson(name, age, eyes) {
-    function talkFunction(msg) {
-        return `I am ${name} and ` + msg;
-    }
-    let person = {n:name, a:age, e:eyes, talk: talkFunction}
+const createPerson = (name, age, eyes) => {
+    let person = {name:name, age:age, eyes:eyes}
+    person.talk = (msg) => `I am ${person.name} and ` + msg;
     return person
 }
 
-raplh = createPerson("Raplh", 33, "brown");
-console.log(raplh.talk("hello there."));
-
 // example of a constructor that builds a Person object.
 // unfinished exercise
-// left at: In this next code listing we do just that by creating a Person function that builds up an object and returns it:
+// left at: In this next code listing we do just that by creating a Person function that builds up an object and returns it
+
+// update 2: just need to type the code from the page but keep my own version because I like it.
